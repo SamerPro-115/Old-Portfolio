47d3034f03de164f3a8a76b7fca3b575aeee6426
Samer <zsamerz.935@gmail.com>
2023-11-17 22:35:57 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import {Container, Navbar,} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import 'animate.css'
+import DarkMood from './components/Navbar/DarkMood'
+import Body from './components/Body/Body.js'
+import Header from './components/Body/Header.js'
+import {FaMoon} from "react-icons/fa";
+import AboutMe from "./components/Body/AboutMe.js"
+import WhatIUseInCoding from "./components/Body/WhatIUseInCoding.js"
+import Works from "./components/Body/Works"
+import ContentCreator from "./components/Body/ContentCreator"
+import {BsMoon} from "react-icons/bs";
+import Animation from './components/Body/Animation';
+
+
+
+
 
 function App() {
+  
+
+  // Dark mood changes
+  const [isDarkMode, setIsDarkMood] = useState(true);
+  
+
+  
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${isDarkMode ? "" : "light-mode-theme"}`}>
+      <Animation isDarkMode={isDarkMode} />
+      <div className='content'>
+      <Navbar expand="lg">
+          <Container className="mt-3">
+            <DarkMood onClick={() => setIsDarkMood(!isDarkMode)}>
+              {isDarkMode ? <FaMoon color="white" />: <BsMoon color="gray" />  }
+            </DarkMood>
+          </Container>
+        </Navbar>
+        <Body>
+           <Header isDarkMode={isDarkMode} />
+           <AboutMe isDarkMode={isDarkMode} />
+           <WhatIUseInCoding isDarkMode={isDarkMode}/>
+           <Works isDarkMode={isDarkMode}/>
+           <ContentCreator isDarkMode={isDarkMode}/>
+        </Body>
+      </div>
     </div>
+
   );
+  
 }
 
 export default App;
