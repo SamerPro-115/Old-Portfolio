d960d6c08c55efa1b70c5b9510f45f28fa494f75
samer132344 <newsamer123@gmail.com>
2023-11-20 06:24:16 +0300

    Fisrt commit

--- src/components/Body/AboutMe.js
+++ src/components/Body/AboutMe.js
@@ -9,7 +9,7 @@ const AboutMe = (prop) => {
         <section className={`animate__animated animate__fadeIn about-me ${isDarkMode ? "" : "light-text"}`}>
 <h1 className="about-me-title">About me ?</h1>
 <p className="about-me-des">
-  My name is Samer Mohammed. I'm a web developer who designs and build
+  My name is Samer Mohammed Alashqar. I'm a web developer who designs and build
   websites. I'm a university student also, I'm a developer since 2021. And not just that ! I'm also a
   content creator on TikTok with over 50k brothers. I'm the founder and creator of the
   "Programming World" Discord server which contains over 500 people
--- src/components/Body/Header.js
+++ src/components/Body/Header.js
@@ -1,7 +1,9 @@
-import {Image} from 'react-bootstrap';
+import {Image, Row, Col} from 'react-bootstrap';
 import {BsTwitter} from "react-icons/bs"
 import { FaTiktok } from "react-icons/fa";
 import {BsDiscord} from "react-icons/bs"
+import { FaYoutube } from "react-icons/fa";
+import { FaCcPaypal } from "react-icons/fa";
 import {useDarkMode } from '../Body/DarkModeContext.js';
 
 
@@ -23,10 +25,16 @@ const Header = () => {
             <BsTwitter className={`animate__animated animate__backInLeft social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
             </a>
             <a href='https://www.tiktok.com/@samerpro_' rel='noreferrer' target='_blank' className='social-button'>
-                <FaTiktok className={`animate__animated animate__backInUp social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
+                <FaTiktok className={`animate__animated animate__backInLeft social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
             </a>
             <a href='https://discord.gg/ZRwX4duYKZ' rel='noreferrer' target='_blank' className='social-button'>
-                <BsDiscord className={`animate__animated animate__backInRight social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
+                <BsDiscord className={`animate__animated animate__backInUp social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
+            </a>
+            <a href='https://www.youtube.com/channel/UCBCgGmpeSb6X__KlJZA_jLw' rel='noreferrer' target='_blank' className='social-button'>
+                <FaYoutube className={`animate__animated animate__backInRight social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
+            </a>
+            <a href='https://www.paypal.com/paypalme/SamerPro935?country.x=SA&locale.x=ar_EG' rel='noreferrer' target='_blank' className='social-button'>
+                <FaCcPaypal className={`animate__animated animate__backInRight social-logo ${isDarkMode ? "" : "social-logo-light"} `} />
             </a>
           </div>
         </div>
